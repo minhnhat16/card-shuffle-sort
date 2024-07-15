dd7951fba895e1bd5965d2ad5b423d9535af1fc2
cyo2 <souderlo1610@gmail.com>
2024-07-15 17:27:19 +0700

    checkpoint

--- Assets/Scripts/UIScript/UI/UI/LoadingView.cs
+++ Assets/Scripts/UIScript/UI/UI/LoadingView.cs
@@ -1,13 +1,11 @@
 using UnityEngine;
 using UnityEngine.UI;
-using Spine.Unity;
 
 public class LoadingView : BaseView
 {
     public Slider loadingProgress;
     public Text loaddingText;
     private float t1 = 0;
-    [SerializeField] SkeletonGraphic logo;
     public override void Setup(ViewParam viewParam)
     {
         base.Setup(viewParam);
@@ -22,23 +20,7 @@ public class LoadingView : BaseView
     {
         UpdateLoadingProgress();
     }
-    private void PlayStartingAnimation()
-    {
-        // Get the AnimationState from the SkeletonAnimation component
-        var animationState = logo.AnimationState;
-
-        // Check if the starting animation exists
-        if (animationState.Data.SkeletonData.FindAnimation("out") != null)
-        {
-            // Set the animation to the starting animation and play it
-            animationState.SetAnimation(0, "out", true);
-        }
-        else
-        {
-            // Log a warning if the starting animation doesn't exist
-            //Debug.LogWarning("Starting animation not found!");
-        }
-    }
+  
     private void UpdateLoadingProgress()
     {
         loadingProgress.value = LoadSceneManager.instance.progress + 0.01f;
